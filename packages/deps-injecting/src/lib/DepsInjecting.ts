45365e3b3d8912d2c311a976f716eed7b62eb4c8
Artur Gubaidullin <lapteuh@gmail.com>
2022-11-27 02:00:54 +0400

    fix func getDistDependencies

--- packages/deps-injecting/src/lib/DepsInjecting.ts
+++ packages/deps-injecting/src/lib/DepsInjecting.ts
@@ -1,12 +1,20 @@
-import { ExecutorContext } from '@nrwl/devkit';
+import {
+  ExecutorContext,
+  ProjectGraph,
+  ProjectGraphProjectNode,
+} from '@nrwl/devkit';
 import { identity, pipe } from 'fp-ts/function';
 import * as IO from 'fp-ts/lib/IO';
 import * as IOE from 'fp-ts/lib/IOEither';
 import * as O from 'fp-ts/lib/Option';
 import * as RA from 'fp-ts/ReadonlyArray';
+import * as RS from 'fp-ts/ReadonlySet';
+import * as S from 'fp-ts/string';
 import * as path from 'path';
 import { getPackageName } from '@embed-dependencies/package-json';
 import { addPackage, publishPackage } from '@embed-dependencies/yalc-client';
+import { Eq, eqStrict } from 'fp-ts/lib/Eq';
+import * as Ord from 'fp-ts/lib/Ord';
 
 export function injectDependencies(
   context: ExecutorContext,
@@ -32,16 +40,41 @@ export function injectDependencies(
 }
 
 function getDistDependencies(context: ExecutorContext): readonly string[] {
+  const EqProjectGraphProjectNode = eqStrict as Eq<ProjectGraphProjectNode>;
+  const OrdProjectGraphProjectNode = Ord.contramap(
+    (a: ProjectGraphProjectNode) => a.name
+  )(S.Ord);
+  const iter = (
+    projectGraph: ProjectGraph,
+    projectName: string,
+    projects: ReadonlySet<ProjectGraphProjectNode>
+  ): ReadonlySet<ProjectGraphProjectNode> =>
+    pipe(
+      projectGraph.dependencies[projectName],
+      RA.map((a) => projectGraph.nodes[a.target]),
+      RA.map(O.fromNullable),
+      RA.compact,
+      RS.fromReadonlyArray(EqProjectGraphProjectNode),
+      RS.difference(EqProjectGraphProjectNode)(projects),
+      RS.reduce(OrdProjectGraphProjectNode)(projects, (acc, v) =>
+        acc.has(v)
+          ? acc
+          : iter(
+              projectGraph,
+              v.name,
+              RS.insert(EqProjectGraphProjectNode)(v)(acc)
+            )
+      )
+    );
+
   return pipe(
     O.Do,
     O.bind('projectName', () => O.fromNullable(context.projectName)),
     O.bind('projectGraph', () => O.fromNullable(context.projectGraph)),
     O.map(({ projectName, projectGraph }) =>
       pipe(
-        projectGraph.dependencies[projectName],
-        RA.map((a) => projectGraph.nodes[a.target]),
-        RA.map(O.fromNullable),
-        RA.compact,
+        iter(projectGraph, projectName, new Set()),
+        RS.toReadonlyArray(OrdProjectGraphProjectNode),
         RA.map((a) => path.join(context.cwd, 'dist', a.data['root']))
       )
     ),
