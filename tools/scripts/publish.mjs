ec08f22edfb59c30dcbc0191c5dbdd38ee032516
Artur Gubaidullin <lapteuh@gmail.com>
2022-12-06 22:16:08 +0400

    Revert "fix: publish script"
    This reverts commit 060de14349c319949a4f2bdc4aaf9d9a2bfe8933.

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -11,6 +11,7 @@
 
 import { readCachedProjectGraph } from '@nrwl/devkit';
 import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
 import chalk from 'chalk';
 
 const DEFAULT_TAG = 'next';
@@ -27,9 +28,16 @@ function invariant(condition, message) {
   }
 }
 
-// Executing publish script: node path/to/publish.mjs {name} --tag {tag}
+// Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
 // Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const [, , name, tag] = process.argv;
+const [, , name, version, tag] = process.argv;
+
+// A simple SemVer validation to validate the version
+const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
+invariant(
+  version && validVersion.test(version),
+  `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
+);
 
 const graph = readCachedProjectGraph();
 const project = graph.nodes[name];
@@ -47,6 +55,17 @@ invariant(
 );
 
 process.chdir(outputPath);
+
+// Updating the version in "package.json" before publishing
+try {
+  const json = JSON.parse(readFileSync(`package.json`).toString());
+  json.version = version;
+  writeFileSync(`package.json`, JSON.stringify(json, null, 2));
+} catch (e) {
+  console.error(
+    chalk.bold.red(`Error reading package.json file from library build output.`)
+  );
+}
 
 // Execute "npm publish" to publish
 execSync(
