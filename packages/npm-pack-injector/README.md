662c12ec8c4c23f185c43be33bff9893c4505942
Artur Gubaidullin <lapteuh@gmail.com>
2023-01-05 22:29:02 +0400

    add npm pack injector

---
+++ packages/npm-pack-injector/README.md
@@ -0,0 +1,11 @@
+# npm-pack-injector
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build npm-pack-injector` to build the library.
+
+## Running unit tests
+
+Run `nx test npm-pack-injector` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/npm-pack-injector/src/lib/NpmPackInjector.ts
@@ -0,0 +1,89 @@
+import {
+  getPackageName,
+  insertDependencies,
+} from '@embed-dependencies/package-json';
+import { ExecutorContext } from '@nrwl/devkit';
+import * as A from 'fp-ts/Array';
+import * as E from 'fp-ts/Either';
+import { constVoid, flow, pipe } from 'fp-ts/function';
+import * as RT from 'fp-ts/ReaderTask';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+import { exec } from 'node:child_process';
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import { promisify } from 'node:util';
+
+const _exec = promisify(exec);
+
+export function injectDependencies(
+  targetPackagePath: string,
+  dependencyPaths: readonly string[]
+): RT.ReaderTask<{ context: ExecutorContext }, void> {
+  return () =>
+    pipe(
+      TE.of(dependencyPaths),
+      TE.chain(TE.traverseArray((a) => pack(a, targetPackagePath))),
+      TE.chainFirst(flow(insertDependencies(targetPackagePath), T.fromIO)),
+      TE.map(constVoid),
+      TE.fold(
+        (e) => async () => {
+          console.error(e);
+        },
+        T.of
+      )
+    );
+}
+
+function pack(
+  sourcePackagePath: string,
+  targetPath: string
+): TE.TaskEither<Error, { name: string; path: string }> {
+  return pipe(
+    TE.of({ sourcePackagePath, targetPath }),
+    TE.bind('packageName', ({ sourcePackagePath }) =>
+      pipe(sourcePackagePath, getPackageName, T.fromIO)
+    ),
+    TE.let('targetPackagePath', ({ packageName }) =>
+      path.join(targetPath, 'libs', packageName)
+    ),
+    TE.chainFirst(({ targetPackagePath }) =>
+      TE.fromTask(async () => {
+        await fs.mkdir(targetPackagePath, { recursive: true });
+      })
+    ),
+    TE.chainFirst(({ targetPackagePath, sourcePackagePath }) =>
+      pipe(
+        async () => {
+          await _exec(`npm pack --pack-destination ${targetPackagePath}`, {
+            cwd: sourcePackagePath,
+          });
+        },
+        (f) => TE.tryCatch(f, E.toError)
+      )
+    ),
+    TE.bind('filename', ({ targetPackagePath }) =>
+      pipe(
+        async () => await fs.readdir(targetPackagePath),
+        TE.fromTask,
+        TE.chain(
+          TE.fromPredicate(
+            A.isNonEmpty,
+            () => new Error('Not found package project file.')
+          )
+        ),
+        TE.map((a) => a[0])
+      )
+    ),
+    TE.let(
+      'relativePackagePath',
+      ({ targetPath, targetPackagePath, filename }) =>
+        'file:' +
+        path.join(path.relative(targetPath, targetPackagePath), filename)
+    ),
+    TE.map((a) => ({
+      name: a.packageName,
+      path: a.relativePackagePath,
+    }))
+  );
+}
